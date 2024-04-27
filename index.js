d39c6d17f02585201c4729d475c95c5a60bb4f5c
Akshit Dayal <akshitdayal99@gmail.com>
2024-04-27 19:55:15 -0400

    Testing - initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const button = document.querySelector('button');
+
+button.addEventListener('click', () => {
+    Notification.requestPermission().then(permission => {
+        if (permission === 'granted') {
+            // setTimeout(() => {
+                const notification = new Notification(
+                    'Example Notif',
+                    {
+                        body: 'Hello World',
+                        data: {
+                            url: 'https://google.com',
+                            hello: 'world',
+                        },
+                        icon: 'https://via.placeholder.com/150',
+                    }
+                );
+
+                notification.addEventListener("error", (error) => {
+                    alert(error);
+                });
+            // }, 2000);
+        }
+    });
+});
