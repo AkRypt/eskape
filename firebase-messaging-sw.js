f03151ed743c01b6ba7af3c2c0dafb39862b9e26
Akshit Dayal <akshitdayal99@gmail.com>
2024-04-28 23:03:32 -0400

    Updates

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,30 @@
+// Scripts for firebase and firebase messaging
+importScripts("https://www.gstatic.com/firebasejs/8.2.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/8.2.0/firebase-messaging-compat.js");
+
+// Initialize the Firebase app in the service worker by passing the generated config
+const firebaseConfig = {
+    apiKey: "AIzaSyCW-i0Sj5CpbDN-50sIdoW6qDu5X-uSI0E",
+    authDomain: "eskape-b391e.firebaseapp.com",
+    projectId: "eskape-b391e",
+    storageBucket: "eskape-b391e.appspot.com",
+    messagingSenderId: "525705863286",
+    appId: "1:525705863286:web:24f7ecabee51532ec5ff57",
+    measurementId: "G-DYDHNJD96Z"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+// Retrieve firebase messaging
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function (payload) {
+    console.log("Received background message ", payload);
+
+    const notificationTitle = payload.notification.title;
+    const notificationOptions = {
+        body: payload.notification.body,
+    };
+
+    self.registration.showNotification(notificationTitle, notificationOptions);
+});
