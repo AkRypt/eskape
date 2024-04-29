36a29631d8cd8fb4d0f74508d3603e63628543ce
Akshit Dayal <akshitdayal99@gmail.com>
2024-04-28 23:21:47 -0400

    Updates

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,30 +1,39 @@
-// Scripts for firebase and firebase messaging
-importScripts("https://www.gstatic.com/firebasejs/8.2.0/firebase-app-compat.js");
-importScripts("https://www.gstatic.com/firebasejs/8.2.0/firebase-messaging-compat.js");
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('../src/firebase-messaging-sw.js')
+        .then(function (registration) {
+            console.log('Registration successful, scope is:', registration.scope);
+        }).catch(function (err) {
+            console.log('Service worker registration failed, error:', err);
+        });
+}
 
-// Initialize the Firebase app in the service worker by passing the generated config
-const firebaseConfig = {
-    apiKey: "AIzaSyCW-i0Sj5CpbDN-50sIdoW6qDu5X-uSI0E",
-    authDomain: "eskape-b391e.firebaseapp.com",
-    projectId: "eskape-b391e",
-    storageBucket: "eskape-b391e.appspot.com",
-    messagingSenderId: "525705863286",
-    appId: "1:525705863286:web:24f7ecabee51532ec5ff57",
-    measurementId: "G-DYDHNJD96Z"
-};
+// // Scripts for firebase and firebase messaging
+// importScripts("https://www.gstatic.com/firebasejs/8.2.0/firebase-app-compat.js");
+// importScripts("https://www.gstatic.com/firebasejs/8.2.0/firebase-messaging-compat.js");
 
-firebase.initializeApp(firebaseConfig);
+// // Initialize the Firebase app in the service worker by passing the generated config
+// const firebaseConfig = {
+//     apiKey: "AIzaSyCW-i0Sj5CpbDN-50sIdoW6qDu5X-uSI0E",
+//     authDomain: "eskape-b391e.firebaseapp.com",
+//     projectId: "eskape-b391e",
+//     storageBucket: "eskape-b391e.appspot.com",
+//     messagingSenderId: "525705863286",
+//     appId: "1:525705863286:web:24f7ecabee51532ec5ff57",
+//     measurementId: "G-DYDHNJD96Z"
+// };
 
-// Retrieve firebase messaging
-const messaging = firebase.messaging();
+// firebase.initializeApp(firebaseConfig);
 
-messaging.onBackgroundMessage(function (payload) {
-    console.log("Received background message ", payload);
+// // Retrieve firebase messaging
+// const messaging = firebase.messaging();
 
-    const notificationTitle = payload.notification.title;
-    const notificationOptions = {
-        body: payload.notification.body,
-    };
+// messaging.onBackgroundMessage(function (payload) {
+//     console.log("Received background message ", payload);
 
-    self.registration.showNotification(notificationTitle, notificationOptions);
-});
+//     const notificationTitle = payload.notification.title;
+//     const notificationOptions = {
+//         body: payload.notification.body,
+//     };
+
+//     self.registration.showNotification(notificationTitle, notificationOptions);
+// });
