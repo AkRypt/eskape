b1300581ffc90170d077e086685e6cff23109e50
Akshit Dayal <akshitdayal99@gmail.com>
2024-04-28 21:00:24 -0400

    added other import scripts

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,3 +1,6 @@
+importScripts('https://www.gstatic.com/firebasejs/<v9+>/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/<v9+>/firebase-messaging-compat.js');
+
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('../src/firebase-messaging-sw.js')
         .then(function (registration) {
